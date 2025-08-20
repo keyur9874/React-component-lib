d88b2230bea4e93c76067abe71d365918a2ab86d
Keyur Asodariya <asodariyakeyur111@gmail.com>
2025-08-20 21:11:25 +0530

    Revert "Remove ununded component"
    This reverts commit 2ac576a86e03a41ea152f8d4906636268e4415e7.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,35 +1,7 @@
-// Button
-export { Button } from './Button/Button';
-export type { ButtonProps } from './Button/Button';
-
-// Avatar
-export { Avatar } from './Avatar/Avatar';
-export type { AvatarProps } from './Avatar/Avatar';
-export { AvatarGroup } from './Avatar/AvatarGroup';
-export type { AvatarGroupProps } from './Avatar/AvatarGroup';
-
-// Badge
-export { Badge } from './Badge/Badge';
-export type { BadgeProps } from './Badge/Badge';
-
-// Input
-export { Input, InputGroup, InputSearch, InputPassword, InputNumber, TextArea } from './Input';
-export type { 
-  InputProps, 
-  InputGroupProps, 
-  InputSearchProps, 
-  InputPasswordProps, 
-  InputNumberProps, 
-  TextAreaProps
-} from './Input';
-
 // Form
-export { FormItem } from './Form';
-export type { FormItemProps } from './Form';
+export { FormItem } from "./Form";
+export type { FormItemProps } from "./Form";
 
 // Switch
-export { Switch } from './Switch';
-export type { SwitchProps } from './Switch';
-// Dropdown
-export { Dropdown } from './Dropdown';
-export type { DropdownProps, DropdownOption, DropdownRef } from './Dropdown';
+export { Switch } from "./Switch";
+export type { SwitchProps } from "./Switch";
